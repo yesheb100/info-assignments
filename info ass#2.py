ba9c1fe72a365dd21428d8bc86add72832fb3401
yesheb100 <104682668+yesheb100@users.noreply.github.com>
2022-05-28 23:40:53 +0500

    Add files via upload

---
+++ info ass#2.py
@@ -0,0 +1,20 @@
+from random import sample
+import rsa
+ 
+
+publicKey, privateKey = rsa.newkeys(512)
+ 
+
+message = open(sample.text,"r")
+ 
+
+encMessage = rsa.encrypt(message.encode(),
+                         publicKey)
+ 
+print("original string: ", message)
+print("encrypted string: ", encMessage)
+ 
+
+decMessage = rsa.decrypt(encMessage, privateKey).decode()
+ 
+print("decrypted string: ", decMessage)
